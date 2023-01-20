5062b553de66ce17cf20d2407496bc6d7e9f6cd1
Anthony Russano <me@anthonyrussano.com>
2023-01-20 03:22:36 -0800

    add route for add user

--- src/app.py
+++ src/app.py
@@ -9,9 +9,30 @@ def index():
 
 @app.route('/get-user', methods=['POST'])
 def get_user():
+    # Get User ID from form input
     user_id = request.form['userId']
     # Set the Qlik Sense server details
-    server = "https://yourserver.com"
+    server = "https://##"
+    virtual_proxy = "your_vp"
+    xrfkey = "abcdefghijklmnoq"
+    # Set the API endpoint and headers
+    endpoint = f"{server}/{virtual_proxy}/qrs/user/{user_id}"
+    headers = {
+        "X-Qlik-xrfkey": xrfkey,
+        "Content-Type": "application/json"
+    }
+    response = requests.get(endpoint, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return response.text
+
+@app.route('/add-user', methods=['POST'])
+def add_user():
+    # Get User ID from form input
+    user_id = request.form['userId']
+    # Set the Qlik Sense server details
+    server = "https://##"
     virtual_proxy = "your_vp"
     xrfkey = "abcdefghijklmnoq"
     # Set the API endpoint and headers
